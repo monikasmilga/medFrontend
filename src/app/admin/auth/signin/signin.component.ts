47c19fee83f38e4df1a1326356e7edf7322f68e4
monikasmilga <tuuli705@gmail.com>
2017-09-11 14:12:19 +0300

    Edited sign in component

--- src/app/admin/auth/signin/signin.component.ts
+++ src/app/admin/auth/signin/signin.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from '../../../auth.service';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-signin',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SigninComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
-
+  onSignin(form: NgForm) {
+    this.authService.signin(form.value.email, form.value.password).subscribe(
+      response => console.log(response),
+        error => console.log(error)
+    );
+  }
 }
