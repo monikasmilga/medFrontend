a0cfbd4117d2074e0563096719574f5e33f534d8
monikasmilga <tuuli705@gmail.com>
2017-09-11 15:05:50 +0300

    Auth service

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,17 +1,35 @@
 import {Injectable} from '@angular/core';
-import {Http, Headers} from '@angular/http';
+import {Http, Headers, Response} from '@angular/http';
 
 import 'rxjs/Rx';
-import { Observable } from 'rxjs';
+import {Observable} from 'rxjs';
 
 @Injectable()
 export class AuthService {
     constructor(private http: Http) {
     }
+
     signin(email: string, password: string) {
         return this.http.post('http://medapp.dev/api/users/signin',
-        {email: email, password: password},
-        {headers: new Headers({'X-Requested-With': 'XMLHttpRequest'})}
+            {email: email, password: password},
+            {headers: new Headers({'X-Requested-With': 'XMLHttpRequest'})}
+        ).map(
+            (response: Response) => {
+                const token = response.json().token;
+                const base64Url = token.split('.')[1];
+                const base64 = base64Url.replace('-', '+').replace('_', '/');
+                return {
+                    token: token, decoded_token: JSON.parse(window.atob(base64))
+                };
+            }
+        ).do(
+            tokenData => {
+                localStorage.setItem('token', tokenData.token);
+            }
         );
+    }
+
+    getToken() {
+        return localStorage.getItem('token');
     }
 }
