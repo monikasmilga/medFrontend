af619893fb7b226a1f82135d6eafceff1f1d3a49
monikasmilga <tuuli705@gmail.com>
2017-09-11 15:32:33 +0300

    Put post and user components under Auth

--- src/app/post.services.ts
+++ src/app/post.services.ts
@@ -2,15 +2,17 @@ import {Injectable} from '@angular/core';
 import {Http, Response} from '@angular/http';
 import 'rxjs/Rx';
 import {Observable} from 'rxjs';
+import {AuthService} from './auth.service';
 
 @Injectable()
 export class PostService {
-    constructor(private http: Http) {
+    constructor(private http: Http, private authService: AuthService) {
 
     }
     // "any" stands for domain which this app communicates with, safety reasons
     getPosts(): Observable<any> {
-        return this.http.get('http://medapp.dev/api/posts').map(
+        const token = this.authService.getToken();
+        return this.http.get('http://medapp.dev/api/posts?token=' + token).map(
             (response: Response) => {
                 return response.json().posts;
             }
--- src/app/user.services.ts
+++ src/app/user.services.ts
@@ -2,15 +2,17 @@ import {Injectable} from '@angular/core';
 import {Http, Response} from '@angular/http';
 import 'rxjs/Rx';
 import {Observable} from 'rxjs';
+import {AuthService} from './auth.service';
 
 @Injectable()
 export class UserService {
-    constructor(private http: Http) {
+    constructor(private http: Http, private authService: AuthService) {
 
     }
     // "any" stands for domain which this app communicates with, safety reasons
     getUsers(): Observable<any> {
-        return this.http.get('http://medapp.dev/api/users').map(
+        const token = this.authService.getToken();
+        return this.http.get('http://medapp.dev/api/users?token=' + token).map(
             (response: Response) => {
                 return response.json().users;
             }
